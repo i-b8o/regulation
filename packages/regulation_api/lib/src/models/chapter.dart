088efa6673bbae3720158efe2d9b24c883e0907b
bogach-ivan <blackview1617@gmail.com>
2022-07-09 21:55:59 +0300

    dsfsdf

--- packages/regulation_api/lib/src/models/chapter.dart
+++ packages/regulation_api/lib/src/models/chapter.dart
@@ -4,11 +4,13 @@ class Chapter {
   final int id;
   final String name;
   final String num;
+  final int orderNum;
   final List<Paragraph> paragraphs;
 
   const Chapter(
       {required this.id,
       required this.name,
       required this.num,
+      required this.orderNum,
       required this.paragraphs});
 }
