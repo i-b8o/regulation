1ed00a21f9265540567896220181170ab3874f82
bogach-ivan <blackview1617@gmail.com>
2022-07-09 21:08:35 +0300

    sdfdsfsf

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -1,11 +1,22 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:regulation/home/cubit/home_cubit.dart';
+import 'package:regulation/three/three.dart';
+import 'package:regulation/two/two.dart';
+
+import '../../one/one.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const HomeView();
+    return BlocProvider(
+      create: (_) => HomeCubit(),
+      child: const HomeView(),
+    );
   }
 }
 
@@ -14,6 +25,107 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center());
+    final selectedTab = context.select((HomeCubit cubit) => cubit.state.tab);
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: const NavigationDrawer(),
+      body: IndexedStack(
+        index: selectedTab.index,
+        children: const [One(), Two()],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        key: const Key('homeView_addTodo_floatingActionButton'),
+        onPressed: () => log('pushed'),
+        child: const Icon(Icons.add),
+      ),
+      // bottomNavigationBar: BottomAppBar(
+      //   shape: const CircularNotchedRectangle(),
+      //   child: Row(
+      //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //     children: [
+      //       _HomeTabButton(
+      //         groupValue: selectedTab,
+      //         value: HomeTab.buy,
+      //         icon: const Icon(Icons.list_rounded),
+      //       ),
+      //       _HomeTabButton(
+      //         groupValue: selectedTab,
+      //         value: HomeTab.choice,
+      //         icon: const Icon(Icons.show_chart_rounded),
+      //       ),
+      //     ],
+      //   ),
+      // ),
+    );
+  }
+}
+
+class _HomeTabButton extends StatelessWidget {
+  const _HomeTabButton({
+    Key? key,
+    required this.groupValue,
+    required this.value,
+    required this.icon,
+  }) : super(key: key);
+
+  final HomeTab groupValue;
+  final HomeTab value;
+  final Widget icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () => context.read<HomeCubit>().setTab(value),
+      iconSize: 32,
+      color:
+          groupValue != value ? null : Theme.of(context).colorScheme.secondary,
+      icon: icon,
+    );
+  }
+}
+
+class NavigationDrawer extends StatelessWidget {
+  const NavigationDrawer({Key? key}) : super(key: key);
+
+  Widget buildHeader(BuildContext context) => Container(
+        padding: EdgeInsets.only(
+          top: MediaQuery.of(context).padding.top,
+        ),
+      );
+  Widget buildMenuItems(BuildContext context) => Column(
+        children: [
+          ListTile(
+            leading: const Icon(Icons.home_outlined, color: Colors.black),
+            title: const Text('Домой'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.home_outlined, color: Colors.black),
+            title: const Text('Домой'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.home_outlined, color: Colors.black),
+            title: const Text('Домой'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.home_outlined, color: Colors.black),
+            title: const Text('Домой'),
+            onTap: () {},
+          ),
+        ],
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: SingleChildScrollView(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [buildHeader(context), buildMenuItems(context)],
+      )),
+    );
   }
 }
---
+++ lib/two/two.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Two extends StatelessWidget {
+  const Two({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
